e81b2a3a22a6939406864eb635eaa52c7498b630
carmauc <carmauc339@hotmail.com>
2022-08-25 14:24:57 -0500

    nuevo hola mundo

---
+++ Holamundo.java
@@ -0,0 +1,8 @@
+class Holamundo {
+    public static void main (String[] args){
+        saludar (nombre: "Mauricio");
+    }
+    public static void saludar (String nombre){
+        System.out.println("Hola" +nombre + "!");
+    }
+}
